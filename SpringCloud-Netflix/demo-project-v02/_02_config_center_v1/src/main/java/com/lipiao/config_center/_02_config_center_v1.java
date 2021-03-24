a693ba3ba26b4ddfa277042b760fcb6016048bfa
13984468001 <13984468001@163.com>
2021-03-25 01:39:24 +0800

    版本 v2 - 配置中心配置文件提交

--- SpringCloud-Netflix/demo-project-v02/_02_config_center_v1/src/main/java/com/lipiao/config_center/_02_config_center_v1.java
+++ SpringCloud-Netflix/demo-project-v02/_02_config_center_v1/src/main/java/com/lipiao/config_center/_02_config_center_v1.java
@@ -13,9 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class _02_config_center_v1 {
-
     public static void main(String[] args) {
         SpringApplication.run(_02_config_center_v1.class, args);
     }
-
 }
